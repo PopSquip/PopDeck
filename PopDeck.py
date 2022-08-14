f4d65b7fa51dd0f599df35727dd3c75fb294e2e6
PopSquip <lukasmetzdorf2008@gmail.com>
2022-08-14 18:19:23 +1000

    Notations

--- PopDeck.py
+++ PopDeck.py
@@ -1,28 +1,29 @@
+#Imports
 import keyboard
 from time import sleep
 
 while True:
 #Apps
-    #Discord
-    if keyboard.is_pressed("PageDown"):
-        keyboard.release("Shift")
-        keyboard.press("Backspace")
-        keyboard.release("Backspace")
-        keyboard.press("Win")
-        keyboard.release("Win")
-        sleep(0.1)
-        keyboard.write("Discord")
-        sleep(0.1)
-        keyboard.press("Enter")
-        keyboard.release("Enter")
-        print("App hotkey one pressed")
+    #App1
+    if keyboard.is_pressed("PageDown"): #Execution Key(s)
+        keyboard.release("Shift")#Keyboard input
+        keyboard.press("Backspace")#Keyboard input
+        keyboard.release("Backspace")#Keyboard input
+        keyboard.press("Win")#Keyboard input
+        keyboard.release("Win")#Keyboard input
+        sleep(0.1)#Wait 0.1 seconds
+        keyboard.write("Discord")#App Name
+        sleep(0.1)#Wait 0.1 seconds
+        keyboard.press("Enter")#Keyboard input
+        keyboard.release("Enter")#Keyboard input
+        print("App hotkey one pressed")#Print
 
 #Overlays
-    #Discord
-    if keyboard.is_pressed("Shift + PageDown"):
-        keyboard.press("Backspace")
-        keyboard.release("Backspace")
-        keyboard.press("Shift")
-        keyboard.write("'")
-        keyboard.release("Shift")
-        print("Overlay hotkey one pressed")
+    #Overlay1
+    if keyboard.is_pressed("Shift + PageDown"):#Execution key(s)
+        keyboard.press("Backspace")#Keyboard input
+        keyboard.release("Backspace")#Keyboard input
+        keyboard.press("Shift")#Keyboard input
+        keyboard.write("'")#Keyboard input
+        keyboard.release("Shift")#Keyboard input
+        print("Overlay hotkey one pressed")#Print
